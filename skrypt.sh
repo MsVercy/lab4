c92050325695da73dc370ee5b5276d4bfdb519bf
MsVercy <m.konatzwolski@gmail.com>
2025-05-07 16:15:23 +0200

    Update files

--- skrypt.sh
+++ skrypt.sh
@@ -11,9 +11,9 @@ if [[ "$1" == "--logs" ]]; then
 fi
 
 if [[ "$1" == "--help" ]]; then
-  echo "--date         Wyświetla dzisiejszą datę"
-  echo "--logs [N]     Tworzy N plików logx.txt (domyślnie 100)"
-  echo "--help         Wyświetla pomoc"
+  echo "-d, --date         Wyświetla dzisiejszą datę"
+  echo "-l, --logs [N]     Tworzy N plików logx.txt (domyślnie 100)"
+  echo "-h, --help         Wyświetla pomoc"
   exit 0
 fi
 
@@ -28,3 +28,17 @@ fi
 if [[ "$1" == "-h" ]]; then
   # Wyświetl pomoc
 fi
+
+if [[ "$1" == "--init" ]]; then
+  git clone https://github.com/TwojLogin/Lab4-GIT.git
+  export PATH="$PATH:$(pwd)/Lab4-GIT"
+fi
+
+if [[ "$1" == "--error" || "$1" == "-e" ]]; then
+  count=${2:-100}
+  for ((i=1; i<=count; i++)); do
+    mkdir -p "error${i}"
+    echo -e "error${i}.txt\nskrypt.sh\n$(date)" > "error${i}/error${i}.txt"
+  done
+fi
+
