1581d226b7046c00af221782ce8cff317b26d3c8
MsVercy <m.konatzwolski@gmail.com>
2025-05-07 15:55:20 +0200

    Add --help option

--- skrypt.sh
+++ skrypt.sh
@@ -9,3 +9,10 @@ if [[ "$1" == "--logs" ]]; then
     echo -e "$filename\nskrypt.sh\n$(date)" > $filename
   done
 fi
+
+if [[ "$1" == "--help" ]]; then
+  echo "--date         Wyświetla dzisiejszą datę"
+  echo "--logs [N]     Tworzy N plików logx.txt (domyślnie 100)"
+  echo "--help         Wyświetla pomoc"
+  exit 0
+fi
