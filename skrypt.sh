a1090e70d5c31a6765b647f0a4882c522dc97bf2
MsVercy <m.konatzwolski@gmail.com>
2025-05-07 15:53:43 +0200

    Add --logs option

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,11 @@
 if [[ "$1" == "--date" ]]; then
 	date
 fi
+
+if [[ "$1" == "--logs" ]]; then
+  count=${2:-100}
+  for ((i=1; i<=count; i++)); do
+    filename="log${i}.txt"
+    echo -e "$filename\nskrypt.sh\n$(date)" > $filename
+  done
+fi
