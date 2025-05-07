dcf983034b20ea09edcdb79bd4f3bac48082f6ba
MsVercy <m.konatzwolski@gmail.com>
2025-05-07 16:01:42 +0200

    Add single dash flags

--- skrypt.sh
+++ skrypt.sh
@@ -16,3 +16,15 @@ if [[ "$1" == "--help" ]]; then
   echo "--help         Wyświetla pomoc"
   exit 0
 fi
+
+if [[ "$1" == "-d" ]]; then date; fi
+if [[ "$1" == "-l" ]]; then
+  count=${2:-100}
+  for ((i=1; i<=count; i++)); do
+    filename="log${i}.txt"
+    echo -e "$filename\nskrypt.sh\n$(date)" > $filename
+  done
+fi
+if [[ "$1" == "-h" ]]; then
+  # Wyświetl pomoc
+fi
